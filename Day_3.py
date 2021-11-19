ddca93ac6e95245e9662ffef000c7b5683c7ad0b
Faton.fa <fatonmaya@gmail.com>
2021-11-19 13:09:15 +0700

    update Day 3

--- Day_3.py
+++ Day_3.py
@@ -6,16 +6,13 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     N = int(input().strip())
     if N%2 != 0 : # if n is odd
         print ('Weird') 
-    else:
-        if  N <=2 and N >= 5:
-            print ('Not Weird') 
-        if N <=6 and N >=20:
-            print ('Weird')
-        if N >= 20:
-            print('Not Weird')
+    elif N >=2 and N <= 5:
+        print ('Not Weird') 
+    elif N >=6 and N <=20:
+        print ('Weird')
+    elif N > 20:
+        print('Not Weird')
